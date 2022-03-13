9c304ecbccef9859e1f4c1536f7de44dc8dbf45d
Praharputra <91005084+Praharputra@users.noreply.github.com>
2022-03-13 10:48:54 +0700

    pbo
    pbo

---
+++ week1/KLingkaran/src/klingkaran/KLingkaran.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package klingkaran;
+
+/**
+ *
+ * @author user
+ */
+public class KLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double keliling;
+        double phi = 3.14;
+        double d = 10;
+
+        // operasi
+        keliling = phi*d;
+
+        //out
+        System.out.println("Keliling Lingkaran :" + keliling);
+    }
+
+}
---
+++ week1/VTabung/src/vtabung/VTabung.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vtabung;
+
+/**
+ *
+ * @author user
+ */
+public class VTabung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double volume;
+        double phi = 3.14;
+        double d = 5;
+        double t = 10;
+
+        
+        volume = phi*d*t;
+
+        System.out.println("Volume tabung : " + volume);
+    }
+
+}
